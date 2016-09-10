eb1d0f02e8fd92bd666bad187d7c99401d0db88b
vsguts <vsguts@gmail.com>
2016-09-10 19:40:27 +0300

    Other files were added.

---
+++ mail/passwordResetToken.php
@@ -0,0 +1,16 @@
+<?php
+use yii\helpers\Html;
+
+/* @var $this yii\web\View */
+/* @var $user common\models\User */
+
+$resetLink = Yii::$app->urlManager->createAbsoluteUrl(['site/reset-password', 'token' => $user->password_reset_token]);
+?>
+
+<?= __('Hello {name}', [
+    'name' => $user->name
+]) ?>!
+
+<?= __('Follow the link below to reset your password') ?>:
+
+<?= Html::a(Html::encode($resetLink), $resetLink) ?>
---
+++ messages/ru-RU/app.php
@@ -0,0 +1,7 @@
+<?php
+
+$messages = [
+    'Powered by' => 'Работает на',
+];
+
+return $messages;
