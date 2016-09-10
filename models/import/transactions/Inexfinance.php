afa13f8617471e590824b618e9b39cd6a51b54c6
vsguts <vsguts@gmail.com>
2016-09-10 20:49:16 +0300

    Import from Inex prototype was added.

--- models/import/transactions/Inexfinance.php
+++ models/import/transactions/Inexfinance.php
@@ -7,7 +7,14 @@ use app\models\BankTransaction;
 
 class Inexfinance extends AbstractProvider
 {
-    
+    protected $currenciesMap = [
+        1 => 1, // USD
+        3 => 4, // RUB
+        6 => 3, // UAH
+    ];
+
+    protected $transactionItems = [];
+
     public function getFormat()
     {
         return 'json';
@@ -15,13 +22,50 @@ class Inexfinance extends AbstractProvider
 
     public function prepareData($data)
     {
-        pd(array_keys($data));
-
-        // TODO
+        // $this->importAccounts($data['resources']);
+        // $this->importClassifications($data['categories']);
+        $this->prepareItems($data['items']);
+        $this->importTransactions($data['transactions']);
+        // $this->importBudgets($data['budgets']); // ??
     }
 
     public function processData($data)
     {
+    }
+
+    protected function importAccounts($items)
+    {
+        pd($items);
+    }
+
+    protected function importClassifications($items)
+    {
+        pd($items);
+    }
+
+    protected function prepareItems($items)
+    {
+        foreach ($items as $item) {
+            $this->transactionItems[$item['itemable_id']]['items'][] = $item;
+        }
+        foreach ($this->transactionItems as $transaction_id => $itemable_data) {
+            $value = 0;
+            $description = [];
+            foreach ($itemable_data['items'] as $item) {
+                $_value = $item['income'] - $item['expense'];
+                $value += $_value;
+                $description[] = $item['name'].': '.$_value;
+            }
+            $this->transactionItems[$transaction_id]['value'] = $value;
+            $this->transactionItems[$transaction_id]['description'] = 'Items:'.PHP_EOL.implode(PHP_EOL, $description);
+            unset($this->transactionItems[$transaction_id]['items']);
+        }
+    }
+
+    protected function importTransactions($items)
+    {
+        pd(array_slice($items, 0, 100), $this->transactionItems);
+        // $transaction =
     }
 
 }
