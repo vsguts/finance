bed4ded6ca8b35344408a35ac839c9d170074d61
vsguts <vsguts@gmail.com>
2017-04-14 11:37:04 +0300

    Calendar: Some fixes

--- components/app/Calendar.php
+++ components/app/Calendar.php
@@ -4,7 +4,7 @@ namespace app\components\app;
 
 use Yii;
 use yii\base\Component;
-use app\models\Weekend;
+use yii\base\Exception;
 
 class Calendar extends Component
 {
@@ -13,30 +13,34 @@ class Calendar extends Component
 
     /**
      * Gets all days in period
-     *
      * @param  mixed $from Timestamp or Date
      * @param  mixed $to   Timestamp or Date
+     * @param string $format
      * @return array
+     * @throws Exception
      */
-    public function getPeriodDays($from, $to)
+    public function getPeriodDays($from, $to, $format = 'params')
     {
         if ($from > $to) {
             list($from, $to) = [$to, $from];
         }
 
-        $formatter = Yii::$app->formatter;
-        $from = $this->dayTimestamp($from);
-        $to = $this->dayTimestamp($to);
-
+        $from = (new \DateTime)->setTimestamp($this->dayTimestamp($from));
+        $to = (new \DateTime)->setTimestamp($this->dayTimestamp($to));
+        $interval = new \DateInterval('P1D'); // one day
+        $to->add($interval);
         $dates = [];
-        $current = $from;
-        while ($current <= $to) {
-            $dates[date('Y-n-j', $current)] = [
-                'year' => date('Y', $current),
-                'month' => date('n', $current),
-                'day' => date('j', $current),
-            ];
-            $current += SECONDS_IN_DAY;
+        foreach (new \DatePeriod($from, $interval, $to) as $step) {
+            $stepTimestamp = $step->getTimestamp();
+            $key = $step->format('Y-n-j');
+            if ($format == 'params') {
+                $day = $this->getTimeData($stepTimestamp);
+            } elseif ($format == 'timestamp') {
+                $day = $stepTimestamp;
+            } else {
+                throw new Exception('Unknown format');
+            }
+            $dates[$key] = $day;
         }
 
         return $dates;
@@ -47,15 +51,19 @@ class Calendar extends Component
      *
      * @param  mixed $from Timestamp or Date
      * @param  mixed $to   Timestamp or Date
+     * @param  string $format
      * @return array
+     * @throws Exception
      */
-    public function getPeriodMonths($from, $to)
+    public function getPeriodMonths($from, $to, $format = 'params')
     {
+        $from = strtotime('first day of this month', $from);
+        $to = strtotime('last day of this month', $to);
+
         if ($from > $to) {
             list($from, $to) = [$to, $from];
         }
 
-        $formatter = Yii::$app->formatter;
         $to = $this->dayTimestamp($to);
 
         $from = (new \DateTime)->setTimestamp($this->dayTimestamp($from));
@@ -64,11 +72,20 @@ class Calendar extends Component
         $period = new \DatePeriod($from, $interval, $to);
 
         $months = [];
-        foreach ($period as $period_step) {
-            $months[] = [
-                'year' => $period_step->format('Y'),
-                'month' => $period_step->format('n'),
-            ];
+        if ($format === 'timestamp') {
+            foreach ($period as $period_step) {
+                $timestamp = mktime(0, 0, 0, $period_step->format('n'), 1, $period_step->format('Y'));
+                $months[$timestamp] = $period_step->format('Y') . ' ' . $period_step->format('F');
+            }
+        } elseif ($format === 'params') {
+            foreach ($period as $period_step) {
+                $months[] = [
+                    'year' => $period_step->format('Y'),
+                    'month' => $period_step->format('n'),
+                ];
+            }
+        } else {
+            throw new Exception('Unknown format');
         }
 
         return $months;
@@ -98,14 +115,19 @@ class Calendar extends Component
      * @param  int $time Timestamp
      * @return int       Timestamp
      */
-    public function dayTimestamp($time)
+    public function dayTimestamp($time = null)
     {
+        if (!$time) {
+            $time = time();
+        }
         $formatter = Yii::$app->formatter;
         return $formatter->asTimestamp($formatter->asDate($time));
     }
 
-    public function getTimestampDateData($timestamp)
+    public function getTimeData($timestamp)
     {
+        $timestamp = Yii::$app->formatter->asTimestamp($timestamp);
+
         return [
             'year' => date('Y', $timestamp),
             'month' => date('n', $timestamp),
