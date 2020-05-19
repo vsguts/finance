44a88bc3f59a9214326478a2f61ddac20956689a
vsguts <vsguts@gmail.com>
2020-05-19 20:15:55 +0300

    YII_ENV and YII_DEBUG env vars added, index.php and app php files were added, init script removed, environments removed.

--- bootstrap/common.php
+++ bootstrap/common.php
@@ -1,35 +1,9 @@
 <?php
 
 require(__DIR__ . '/../vendor/autoload.php');
-require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 require(__DIR__ . '/components/functions.php');
 
-/**
- * Global config
- */
-
-ini_set('memory_limit', '512M');
-
-
-/**
- * Dirs and base classes config
- */
-
 $root = dirname(__DIR__);
-Yii::setAlias('@root', $root);
-Yii::setAlias('@storage', $root . '/storage');
-
-class_alias('\yii\helpers\Html', 'Html');
-class_alias('\yii\helpers\Url', 'Url');
-
-
-/**
- * Constants
- */
-
-define('SECONDS_IN_DAY', 24 * 60 * 60);
-define('SECONDS_IN_YEAR', SECONDS_IN_DAY * 365);
-
 
 /**
  * Env vars
@@ -37,3 +11,35 @@ define('SECONDS_IN_YEAR', SECONDS_IN_DAY * 365);
 
 $dotenv = \Dotenv\Dotenv::create($root);
 $dotenv->load();
+
+/**
+ * Global config
+ */
+
+defined('YII_DEBUG') || define('YII_DEBUG', env('YII_DEBUG'));
+defined('YII_ENV') || define('YII_ENV', env('YII_ENV'));
+
+ini_set('memory_limit', '512M');
+
+/**
+ * Start Yii Base Logic
+ */
+
+require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
+
+/**
+ * Dirs and base classes config
+ */
+
+Yii::setAlias('@root', $root);
+Yii::setAlias('@storage', $root . '/storage');
+
+class_alias('\yii\helpers\Html', 'Html');
+class_alias('\yii\helpers\Url', 'Url');
+
+/**
+ * Constants
+ */
+
+define('SECONDS_IN_DAY', 24 * 60 * 60);
+define('SECONDS_IN_YEAR', SECONDS_IN_DAY * 365);
